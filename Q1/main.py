5540ee566a47c37e35c7f6420459340faf3e57da
Hugo Silva <vitorhugo.dev.go@gmail.com>
2026-07-03 10:06:04 -0300

    Exercício 03 resolvido.

--- Q1/main.py
+++ Q1/main.py
@@ -13,12 +13,12 @@ class ProgressaoAritmetica:
         self.a1 = a1
         self.r = r
 
-    def somar(self) -> int:
+    def somar(self) -> float:
         # ultimo_t = primeiro_t + razão * (qnt_t - 1)
         ultimo_termo = self.a1 + self.r * (self.n - 1)
         return self.n * (self.a1 + ultimo_termo) / 2
     
-    def gerar_termos(self) -> list[int]:
+    def gerar_termos(self) -> list[float]:
         termos = [self.a1]
         value = self.a1
         for _ in range(self.n - 1):
--- Q3/main.py
+++ Q3/main.py
@@ -4,3 +4,34 @@ uma frase dada pelo usuário e a criptografe. A criptografia
 consistirá na troca das vogais da frase por um número
 correspondente: A – 4, E – 3, I – 1, O – 0, U – 8.
 '''
+
+class Secret:
+    def __init__(self, subject: str) -> None:
+        self.subject = subject
+        self.vowels = {
+            "a": 4,
+            "e": 3,
+            "i": 1,
+            "o": 0,
+            "u": 8
+        }
+    
+    def encrypt(self) -> str:
+        temp_subject = ""
+        for s in self.subject.lower():
+            try:
+                temp_subject += str(self.vowels[s])
+            except KeyError:
+                temp_subject += s
+        self.subject = temp_subject
+        return self.subject
+    
+    def decrypt(self) -> str:
+        for s, c in self.vowels.items():
+            self.subject = self.subject.replace(str(c), s)
+        return self.subject
+
+if __name__ == "__main__":
+    segredo = Secret(input("Digite um texto: "))
+    print("\nTexto criptografado:   ", segredo.encrypt())
+    print("\nTexto descriptografado:", segredo.decrypt())
